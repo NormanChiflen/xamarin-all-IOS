2c06bb6bde1705eb5cdc04347f329c9d68ce7e15
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-09-21 19:12:23 +0200

    [generator] Add support for C#'s nint/nuint types

--- src/generator.cs
+++ src/generator.cs
@@ -2562,6 +2562,7 @@ public partial class Generator : IMemberGatherer {
 						case "MarshalDirectiveAttribute":
 						case "BindingImplAttribute":
 						case "XpcInterfaceAttribute":
+						case "NativeIntegerAttribute":
 							continue;
 						default:
 							throw new BindingException (1007, true, attr.GetType (), mi.DeclaringType, mi.Name);
@@ -7364,7 +7365,13 @@ public partial class Generator : IMemberGatherer {
 										}
 									}
 
-									print ("return {0}!;", def);
+									if (mi.ReturnType == TypeManager.System_nint) {
+										print ("return ((nint) ({0}));", def);
+									} else if (mi.ReturnType == TypeManager.System_nuint) {
+										print ("return ((nuint) ({0}));", def);
+									} else {
+										print ("return {0}!;", def);
+									}
 								}
 							}
 						}
